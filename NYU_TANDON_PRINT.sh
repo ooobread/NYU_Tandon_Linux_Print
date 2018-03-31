c0a9304b1b760b672c9acc08ead570d7d7a563fb
Wei Lin <123333ster@gmail.com>
2018-03-31 19:46:56 -0400

    Slightly Changes.

---
+++ NYU_TANDON_PRINT.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install cups printer-driver-hpcups printer-driver-postscript-hp python python-wxgtk3.0 python-wxtools wx3.0-i18n libwxgtk3.0-dev libgtk2.0-dev
+rm -rf ~/pharos && mkdir ~/pharos && pushd ~/pharos
+wget -c https://github.com/junaidali/pharos-linux/archive/master.zip && unzip master.zip && rm master.zip && cd pharos-linux-master
+wget http://pastebin.com/raw.php?i=pR3gLYFw -O- | tr -d '\r' > printers.conf
+sed -i -e 's/Please enter your myIIT ID/Please enter your NYU NetID/g' pharospopup
+sudo python setup.py
+rm -rf ../pharos-linux-master
+popd > /dev/null && sudo rm -r ~/pharos
